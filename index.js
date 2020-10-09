8fab125b82f7d27bafc93fab8b5aca7075e512e9
yasirshahid <yasirshahid5@gmail.com>
2020-10-09 13:28:13 +0500

    update mutation

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const student = [
-    {
-        name: "Dua",
-        email: "dua@gmail.com",
-        age: 6,
-        id: 0
-    },
-    {
-        name: "Aarib",
-        email: "aarib@gmail.com",
-        age: 2,
-        id: 1
-    },
-    {
-        name: "yasir",
-        email: "yasir@gmail.com",
-        age: 35,
-        id: 3
-    }
+  {
+    name: "Dua",
+    email: "dua@gmail.com",
+    age: 6,
+    id: 0
+  },
+  {
+    name: "Aarib",
+    email: "aarib@gmail.com",
+    age: 2,
+    id: 1
+  },
+  {
+    name: "yasir",
+    email: "yasir@gmail.com",
+    age: 35,
+    id: 3
+  }
 ];
 
 const resolvers = {
-    Query: {
-      student: () => student,
-    },
-  };
+  Query: {
+    student: () => student,
+  },
+  Mutation: {
+    addStudent: (_, { input }) => {
+      console.log(input.name);
+      return {
+        name: input.name,
+        age: input.age,
+        email: input.email,
+        id: input.id
+
+      }
+    }
+  }
+};
 const typeDefs = gql`
   
-  type Students {
+  type Student {
+    name: String
+    email: String
+    age: Int
+    id: Int
+  }
+  
+  input StdInput {
     name: String
     email: String
     age: Int
@@ -37,9 +56,14 @@ const typeDefs = gql`
 
   
   type Query {
-    student: [Students]
+    student: [Student]
+  }
+
+  type Mutation {
+    addStudent (input: StdInput): Student
   }
 `;
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
